ff6679b363c830a7c3c0848aabc3e6c4a57a84e6
auddiolafs <32578333+auddiolafs@users.noreply.github.com>
2026-04-16 09:26:37 +0000

    correct license filename

--- ports/re2c/portfile.cmake
+++ ports/re2c/portfile.cmake
@@ -9,4 +9,4 @@ vcpkg_from_github(
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
