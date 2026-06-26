e6f93bf824726162cecbab4be2ac56f92f93e242
Daniel Preineder <30234187+DanielPreineder@users.noreply.github.com>
2026-06-26 12:16:56 +0000

    Update ref

--- ports/carbon-videoplayer/portfile.cmake
+++ ports/carbon-videoplayer/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH
   URL git@github.com:carbonengine/videoplayer.git
-  REF 45048628c2c7cefc339fb676f2644c18f4b87052
+  REF 2c4ac902d1474238097e70ab5c6e7f4bf7bc6755
   HEAD_REF main
 )
 
