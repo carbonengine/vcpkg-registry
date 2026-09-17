de8cb29af8e9898209545148098968d990fef15e
Filipp Pavlov <filipp@ccpgames.com>
2026-09-17 08:26:22 +0000

    Add a toolchain for trinitydev configs that disables LTCG for v145

---
+++ toolchains/x64-windows-145-trinitydev-triplet.cmake
@@ -0,0 +1,7 @@
+# Copyright © 2025 CCP ehf.
+# This toolchain is meant for use inside a vcpkg triplet. See `README.md` for more details.
+include($ENV{PATH_TO_VCPKG_ROOT}/scripts/toolchains/windows.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../toolchains/x64-windows-145-carbon.cmake)
+
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION OFF)
+add_link_options("/LTCG:OFF")
